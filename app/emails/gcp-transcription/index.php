c5ec982a02cc8d9e872f878c8dceb0b9202e1d00
Christian King <christian.king@gmail.com>
2019-09-22 18:16:23 -0400

    Changed to optionally use gcs to allow for async transcription
    The default transcription only allows for up to ~1 minute of audio
    but the async call requires the audio to be copied out to cloud
    storage first.  A new setting in voicemail -> transcription_bucket
    holds the value of the bucket to store the audio in for the
    transcription.

--- app/emails/gcp-transcription/index.php
+++ app/emails/gcp-transcription/index.php
@@ -1,15 +1,15 @@
 <?php
 
-// Include Google Cloud dependendencies using Composer
+// Include Google Cloud dependencies using Composer
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Google\Cloud\Speech\V1\SpeechClient;
 use Google\Cloud\Speech\V1\RecognitionAudio;
 use Google\Cloud\Speech\V1\RecognitionConfig;
 use Google\Cloud\Speech\V1\RecognitionConfig\AudioEncoding;
+use Google\Cloud\Storage\StorageClient;
 
 function get_transcription($attachment_str, $filename) {
-	
 
 	$enabled = false;
 	if (isset($_SESSION['voicemail']['transcription']) && $_SESSION['voicemail']['transcription']['boolean'] == "true") {
@@ -20,56 +20,64 @@ function get_transcription($attachment_str, $filename) {
 		return "";
 	}
 
+    $tmp_filename = "/tmp/vm_".microtime(1);
+    $vm_ext = pathinfo($filename, PATHINFO_EXTENSION);
 	try {
 		openlog("FusionPBX", LOG_PID | LOG_PERROR, LOG_LOCAL0);
-		$tmp_filename = "/tmp/vm_".microtime(1);
-		$vm_ext = pathinfo($filename, PATHINFO_EXTENSION);
 
 		file_put_contents("$tmp_filename.$vm_ext", $attachment_str);
 		system("sox $tmp_filename.$vm_ext $tmp_filename.flac rate 8k");
 
-		$content = file_get_contents($tmp_filename.".flac");
+		if (isset($_SESSION['voicemail']['transcription_bucket']) && $_SESSION['voicemail']['transcription_bucket']) {
+		    // If we have a bucket to store to, save to the bucket temporarily to use the async api
+            // to support voicemails over 1 minute
+            $object_name = $tmp_filename . ".flac";
 
-		// Clean up temporary files
-		@unlink("$tmp_filename.$vm_ext");
-		@unlink("$tmp_filename.flac");
+            $bucket = (new StorageClient())
+                ->bucket($_SESSION['voicemail']['transcription_bucket']);
 
-		$audio = (new RecognitionAudio())
-		    ->setContent($content);
+            $file = fopen($tmp_filename, 'r');
+            $object = $bucket->upload($file, [ 'name' => $object_name ]);
 
-		$config = (new RecognitionConfig())
-		    ->setEncoding(AudioEncoding::FLAC)
-		    ->setSampleRateHertz(8000)
-		    ->setLanguageCode('en-US')
-		    ->setUseEnhanced(true)
-		    ->setModel('phone_call');
+            $audio = (new RecognitionAudio())
+                ->setUri($object->gcsUri());
+        } else {
+            $content = file_get_contents($tmp_filename.".flac");
+            $audio = (new RecognitionAudio())
+                ->setContent($content);
+        }
 
-		$client = new SpeechClient();
+        $config = (new RecognitionConfig())
+            ->setEncoding(AudioEncoding::FLAC)
+            ->setSampleRateHertz(8000)
+            ->setLanguageCode('en-US')
+            ->setUseEnhanced(true)
+            ->setModel('phone_call');
 
-		$response = $client->recognize($config, $audio);
-		$results = $response->getResults();
-		$transcription = '';
+        $client = new SpeechClient();
 
-		if ($results[0]) {
-			$transcription = $results[0]->getAlternatives()[0]->getTranscript();
-		}
+        $op = $client->longRunningRecognize($config, $audio);
+        $op->pollUntilComplete();
+
+        $transcription = "";
+        if ($op->operationSucceeded()) {
+            $response = $op->getResult();
+            foreach ($response->getResults() as $result) {
+                $transcription .= $result->getAlternatives()[0]->getTranscript();
+            }
+        } else {
+            syslog(LOG_WARNING, "Error returned during transcription: " . $op->getError()->getMessage());
+        }
+
 		$client->close();
 		return $transcription;
 	} catch (Exception $e) {
 		syslog(LOG_WARNING, "Error during transcription: " . $e->getMessage());
 	} finally {
-		closelog();
-	}
-}
+        // Clean up temporary files
+        @unlink("$tmp_filename.$vm_ext");
+        @unlink("$tmp_filename.flac");
 
-function __gcp_get_vm_ext() {
-	$sql = "select * from v_vars where var_name = 'vm_message_ext'";
-	$pre_statement = $db->prepare(check_sql($sql));
-	$pre_statement->execute();
-	$result = $prep_statement->fetchAll(PDO::FETCH_NAMED);
-	foreach ($result as &$row) {
-		return $row["var_value"];
+        closelog();
 	}
-	if ($var_value) return $var_value;
-	return "wav";
 }
