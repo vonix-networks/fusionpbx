1c65ff618ec348285eba77896c270fe4cc64237f
root <root@prod-east1-pbx1-d.us-east1-d.c.vonix-prod.internal>
2020-04-05 01:05:39 +0000

    Changing logo to Vonix

--- themes/default/app_config.php
+++ themes/default/app_config.php
@@ -7,7 +7,7 @@
 		$apps[$x]['subcategory'] = "";
 		$apps[$x]['version'] = "1.0";
 		$apps[$x]['license'] = "Mozilla Public License 1.1";
-		$apps[$x]['url'] = "http://www.fusionpbx.com";
+		$apps[$x]['url'] = "http://www.vonix.io";
 		$apps[$x]['description']['en-us'] = "Default theme";
 		$apps[$x]['description']['en-gb'] = "Default theme";
 		$apps[$x]['description']['ar-eg'] = "";
