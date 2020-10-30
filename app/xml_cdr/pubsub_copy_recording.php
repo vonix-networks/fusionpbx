548d77bb94cc7aae09631bfd039d3d49c3781af1
Christian King <cking@vonix.io>
2020-10-30 13:19:03 -0400

    Switched to php://input to read message body

--- app/xml_cdr/pubsub_copy_recording.php
+++ app/xml_cdr/pubsub_copy_recording.php
@@ -7,7 +7,7 @@ require_once "resources/require.php";
 try {
     $processor = new copy_recording_processor;
 
-    $processor->process($_POST);
+    $processor->process(file_get_contents('php://input'));
 } catch(Exception $e) {
     header('HTTP/1.1 401 Unauthorized');
     die('Invalid token');
